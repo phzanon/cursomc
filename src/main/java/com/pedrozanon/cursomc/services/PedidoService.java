d47e15a1ea1ca81f8e52ba224580ff4427b9c95a
Pedro Zanon <p.hzanon123@gmail.com>
2019-04-03 17:10:53 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/pedrozanon/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.pedrozanon.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrozanon.cursomc.domain.Pedido;
+import com.pedrozanon.cursomc.exceptions.ObjectNotFoundException;
+import com.pedrozanon.cursomc.repositories.PedidoRepository;
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
