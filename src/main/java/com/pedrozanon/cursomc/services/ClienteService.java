5f66b22f54b68e935d2e726b4fe50bb63238e227
Pedro Zanon <p.hzanon123@gmail.com>
2019-04-03 11:12:54 -0300

    endpoint /clientes/{id} disponiveis

---
+++ src/main/java/com/pedrozanon/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.pedrozanon.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedrozanon.cursomc.domain.Categoria;
+import com.pedrozanon.cursomc.domain.Cliente;
+import com.pedrozanon.cursomc.exceptions.ObjectNotFoundException;
+import com.pedrozanon.cursomc.repositories.ClienteRepository;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+}
