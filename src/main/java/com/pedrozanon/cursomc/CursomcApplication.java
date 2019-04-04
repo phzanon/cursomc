7708a068f8286cd4df1128dea4c3471f5d632025
Pedro Zanon <p.hzanon123@gmail.com>
2019-04-04 11:26:53 -0300

    Paginação com parametros opcionais

--- src/main/java/com/pedrozanon/cursomc/CursomcApplication.java
+++ src/main/java/com/pedrozanon/cursomc/CursomcApplication.java
@@ -70,6 +70,17 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Teste1");
+		Categoria cat4 = new Categoria(null, "Teste4");
+		Categoria cat5 = new Categoria(null, "Teste5");
+		Categoria cat6 = new Categoria(null, "Teste6");
+		Categoria cat7 = new Categoria(null, "Teste7");
+		Categoria cat8 = new Categoria(null, "Teste8");
+		Categoria cat9 = new Categoria(null, "Teste9");
+		Categoria cat10 = new Categoria(null, "Teste10");
+		Categoria cat11 = new Categoria(null, "Teste11");
+		Categoria cat12 = new Categoria(null, "Teste12");
+		Categoria cat13 = new Categoria(null, "Teste13");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
@@ -82,7 +93,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		repo.saveAll(Arrays.asList(cat1,cat2));
+		repo.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10,cat11,cat12,cat13));
 		pr.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
--- src/main/java/com/pedrozanon/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedrozanon/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pedrozanon.cursomc.domain.Categoria;
@@ -49,5 +52,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		List<Categoria> lista = repo.findAll();
 		return lista;
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String dir) {
+		PageRequest pageReq = PageRequest.of(page, linesPerPage, Direction.valueOf(dir), orderBy);
+		return repo.findAll(pageReq);
 	}
 }
