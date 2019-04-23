cc8ce41a61dbd28d1f5e766ea98a80bdfa7fa847
Pedro Zanon <p.hzanon123@gmail.com>
2019-04-23 17:58:44 -0300

    Enviando email com HTML

--- src/main/java/com/pedrozanon/cursomc/services/PedidoService.java
+++ src/main/java/com/pedrozanon/cursomc/services/PedidoService.java
@@ -75,7 +75,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		ipRepo.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }
